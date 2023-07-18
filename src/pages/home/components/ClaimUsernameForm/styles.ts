dc63ec738ff6d14202531eefbe2eb27edba05dcd
bruno <brmartins92@gmail.com>
2023-07-18 01:02:27 -0300

    input reservar

---
+++ src/pages/home/components/ClaimUsernameForm/styles.ts
@@ -0,0 +1,9 @@
+import { Box, TextInput, styled } from "@ignite-ui/react";
+
+export const Form = styled(Box,{
+ display: 'grid',
+ gridTemplateColumns: '1fr',
+ gap: '$2',
+ marginTop: '$4',
+ padding: '$4',
+})
