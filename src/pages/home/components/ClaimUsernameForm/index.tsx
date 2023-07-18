6b5bbae62696402fc164e11a8260279365d65461
bruno <brmartins92@gmail.com>
2023-07-18 01:17:28 -0300

    add zod

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,11 +1,29 @@
 import { Button, TextInput } from "@ignite-ui/react";
 import { Form } from "./styles";
 import { Airplane } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const ClaimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function HandleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data.username);
+  }
+
   return (
-    <Form> 
-      <TextInput size="sm" prefix="ignite.com" placeholder="seu-usuario"/>
+    <Form as="form" onSubmit={handleSubmit(HandleClaimUsername)}> 
+      <TextInput size="sm" 
+        prefix="ignite.com" 
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
       <Button size="sm" type="submit"> 
         Reservar Usuario 
         <Airplane size={32} />
